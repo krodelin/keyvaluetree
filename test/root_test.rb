4a39a5290c104a6ae583e2ec11b0081ae07b9650
Udo Schneider <Udo.Schneider@homeaddress.de>
2013-08-31 15:26:09 +0200

    Added Root test

--- test/root_test.rb
+++ test/root_test.rb
@@ -1,3 +1,24 @@
-class RootTest
-  # To change this template use File | Settings | File Templates.
+require "common"
+
+class RootTest < Test::Unit::TestCase
+
+  context "KeyValueTree" do
+
+    setup do
+      @hash_object ={:a => :b}
+      @array_object = [:a, :b]
+      @store = KeyValueTree::MemoryStore.new()
+    end
+
+    should "be created from an object" do
+      assert_nothing_raised(Exception) { KeyValueTree.from(@hash_object) }
+    end
+
+    should "be created from an object with store" do
+      assert_nothing_raised(Exception) { KeyValueTree.from(@hash_object, @store) }
+    end
+
+
+  end
+
 end
